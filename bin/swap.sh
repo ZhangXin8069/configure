f3b4660c1762236863b820c6559916247157229d
zhangxin <zhangxin8069@qq.com>
2023-09-12 19:06:46 +0800

    2023-09-12-19-06-46

--- bin/swap.sh
+++ bin/swap.sh
@@ -3,4 +3,4 @@ sudo dd if=/dev/zero of=/var/swapfile bs=1M count=16384
 sudo mkswap /var/swapfile
 sudo swapon /var/swapfile
 # sudo echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab
-# sudo sysctl vm.swappiness=10
+# sudo sysctl vm.swappiness=0
