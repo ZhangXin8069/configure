a8beefef5e2822837c65ef421ad0a372d265339f
zhangxin <zhangxin8069@qq.com>
2023-10-27 13:50:32 +0800

    2023-10-27-13-50-32

--- tmp/work6.sh
+++
@@ -1,3 +0,0 @@
-pushd /home/zhangxin/configure/tmp
-nvcc -o work6 /home/zhangxin/configure/test/work6.cu && ./work6
-popd
--- tmp/work8.sh
+++
@@ -1,3 +0,0 @@
-pushd /home/zhangxin/configure/tmp
-nvcc -o work8 /home/zhangxin/configure/test/work8.cu && ./work8
-popd
