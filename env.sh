3bcbdc7b8a51675dc4a574939a87647efe73a5a5
zhangxin <zhangxin8069@qq.com>
2024-02-25 11:38:53 +0800

    2024-02-25-11-38-53

--- env.sh
+++ env.sh
@@ -21,15 +21,22 @@ source ${_HOME}/tmp/scripts.sh
 
 # do
 ## export
-export LD_LIBRARY_PATH=${HOME}/lib:$LD_LIBRARY_PATH
+### zx
+export LD_LIBRARY_PATH=${HOME}/lib:$LD_LIBRARY_PATH # if any
+export TERM=xterm-256color
+export PATH=$PATH:${_HOME}/bin
+export PATH=$PATH:/home/zhangxin/.local/bin
+### openmpi
 MPI_HOME=/usr/local/openmpi
 export PATH=${MPI_HOME}/bin:$PATH
 export LD_LIBRARY_PATH=${MPI_HOME}/lib:$LD_LIBRARY_PATH
 export MANPATH=${MPI_HOME}/share/man:$MANPATH
+### cuda
 CUDA_HOME=/usr/local/cuda
 export PATH=${CUDA_HOME}/bin:$PATH
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib:$LD_LIBRARY_PATH
 export MANPATH=${CUDA_HOME}/share/man:$MANPATH
-export TERM=xterm-256color
-export PATH=$PATH:${_HOME}/bin
-export PATH=$PATH:/home/zhangxin/.local/bin
+export LD_LIBRARY_PATH="/usr/lib/wsl/lib:${LD_LIBRARY_PATH}" # wsl
+
+
+
