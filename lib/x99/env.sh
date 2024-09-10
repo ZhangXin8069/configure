b7b482b8f5abb2b3acb0e2b1ebecd37b95b670ce
zhangxin <zhangxin8069@qq.com>
2024-09-10 23:41:38 +0800

    2024-09-10-23-41-38

--- lib/x99/env.sh
+++ lib/x99/env.sh
@@ -1,11 +1,11 @@
 # BASH
 # unset
 
+# ALIAS
+alias ncu="nv-nsight-cu-cli"
+alias ncu-ui="nv-nsight-cu"
+
 # SOURCE
 pushd /home/kfutfd/qcu
 source ./x99-env.sh
 popd
-
-# ALIAS
-alias nv-nsight-cu-cli="ncu"
-alias nv-nsight-cu="ncu-ui"
