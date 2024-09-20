6124be26d61399ba3d25754f4e2c3f234b0a3331
zhangxin <zhangxin8069@qq.com>
2024-09-20 19:39:27 +0800

    2024-09-20-19-39-27

--- lib/aistudio/env.sh
+++ lib/aistudio/env.sh
@@ -1,10 +1,18 @@
 pushd ~
 echo "
+## zhangxin
+export NCCL_DEBUG=
+# export NCCL_DEBUG=INFO
 export PATH=\$PATH:/home/aistudio/external-libraries/sbin
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/aistudio/external-libraries/slib
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/aistudio/qcu/lib
 export PYTHONPATH=\$PYTHONPATH:/home/aistudio/external-libraries
+export PYTHONPATH=\$PYTHONPATH:/home/aistudio/qcu/lib
 export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/aistudio/external-libraries/quda-develop/build/lib
-source ~/work/configure/env.sh
-source ~/work/qcu/env.sh
+source /home/aistudio/configure/env.sh
+## quda
+export QUDA_ENABLE_P2P=0
+export QUDA_ENABLE_TUNING=0
 " >> ./.bashrc
 source ./.bashrc
 popd
