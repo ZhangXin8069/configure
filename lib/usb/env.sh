bd58d29916b3877dda33dec9d286fde606ddaf75
zhangxin <zhangxin8069@qq.com>
2024-09-24 17:07:25 +0800

    2024-09-24-17-07-25

--- lib/usb/env.sh
+++ lib/usb/env.sh
@@ -6,10 +6,28 @@ export PATH=${HOME}/sbin:$PATH
 export LD_LIBRARY_PATH=${HOME}/slib:$LD_LIBRARY_PATH
 
 # ALIAS
+#########
+# EXAMPLE
+# sudo apt install flatpak
+# flatpak install flathub
+# flatpak run com.tencent.WeChat
+# flatpak run com.tencent.WeChat
+# in ${HOME}/.local/share/flatpak/exports/bin
+# in /var/lib/flatpak/exports/bin
+#########
 alias wechat="flatpak run com.tencent.WeChat"
 alias wemeet="flatpak run com.tencent.wemeet"
 alias qq="flatpak run com.qq.QQ"
 alias joplin="flatpak run net.cozic.joplin_desktop"
+alias code="flatpak run com.visualstudio.code --no-sandbox"
+alias krita="flatpak run org.kde.krita"
+alias firefox="flatpak run org.mozilla.firefox"
+alias goldendict="flatpak run io.github.xiaoyifang.goldendict_ng"
+alias media_writer="flatpak run org.fedoraproject.MediaWriter"
+#########
+# EXAMPLE
+# http://dict.youdao.com/search?q=%GDWORD%&ue=utf8
+#########
 
 # SOURCE
 pushd ${HOME}/qcu
