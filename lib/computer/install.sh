27f5f812ced33630bb9be3dbcb0b527499d50dcd
zhangxin <zhangxin8069@qq.com>
2024-10-19 22:50:33 +0800

    2024-10-19-22-50-32

--- lib/computer/install.sh
+++ lib/computer/install.sh
@@ -17,6 +17,7 @@ conda install conda-forge::cuda-toolkit=12.4
 conda install conda-forge::cupy=13.3.0
 conda install conda-forge::nccl=2.23.4
 conda install conda-forge::Cython=3.0.11
+conda install conda-forge::git=2.47.0
 pushd ~/miniconda3/envs/qcu
 wget https://gitee.com/zhangxin8069/quda_packages/raw/main/quda-develop.tar.gz
 tar -xzf quda-develop.tar.gz
@@ -26,6 +27,9 @@ mkdir build -p && pushd build
 cmake .. -DQUDA_DIRAC_DOMAIN_WALL=OFF -DQUDA_CLOVER_DYNAMIC=OFF -DQUDA_CLOVER_RECONSTRUCT=OFF -DQUDA_DIRAC_NDEG_TWISTED_CLOVER=OFF -DQUDA_DIRAC_NDEG_TWISTED_MASS=OFF -DQUDA_DIRAC_TWISTED_CLOVER=OFF -DQUDA_DIRAC_TWISTED_MASS=OFF -DQUDA_INTERFACE_MILC=OFF -DQUDA_LAPLACE=ON -DQUDA_MULTIGRID=ON -DQUDA_GPU_ARCH=sm_80 -DQUDA_MPI=ON
 cmake --build . -j6
 cmake --install .
+pushd ./lib
+ln -s ../quda/build/lib/libquda.so .
+popd
 popd
 popd
 popd
