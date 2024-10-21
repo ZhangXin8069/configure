3f2177d90580c27206d6cf2aa4a5f1660e8fb8fb
zhangxin <zhangxin8069@qq.com>
2024-10-21 14:44:02 +0000

    2024-10-21-14-44-02

--- lib/v20241023/setup.sh
+++ lib/v20241023/setup.sh
@@ -29,7 +29,18 @@ popd
 git clone https://github.com/NVIDIA/nccl-tests.git
 pushd ./nccl-tests
 make
-./build/all_reduce_perf -b 8 -e 256M -f 2 -g <ngpus>
+./build/all_reduce_perf -b 8 -e 256M -f 2 -g 1
+# ./build/all_reduce_perf -b 8 -e 256M -f 2 -g <ngpus>
+popd
+wget https://gitee.com/zhangxin8069/quda_packages/raw/main/quda-develop.tar.gz
+tar -xzf quda-develop.tar.gz
+mv quda-develop quda
+pushd ./quda
+mkdir build -p && pushd build
+cmake .. -DQUDA_DIRAC_DOMAIN_WALL=OFF -DQUDA_CLOVER_DYNAMIC=OFF -DQUDA_CLOVER_RECONSTRUCT=OFF -DQUDA_DIRAC_NDEG_TWISTED_CLOVER=OFF -DQUDA_DIRAC_NDEG_TWISTED_MASS=OFF -DQUDA_DIRAC_TWISTED_CLOVER=OFF -DQUDA_DIRAC_TWISTED_MASS=OFF -DQUDA_INTERFACE_MILC=OFF -DQUDA_LAPLACE=ON -DQUDA_MULTIGRID=ON -DQUDA_GPU_ARCH=sm_80 -DQUDA_MPI=ON
+cmake --build . -j6
+cmake --install .
+popd
 popd
 popd
 ######
