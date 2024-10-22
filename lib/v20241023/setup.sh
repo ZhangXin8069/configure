671deb13f7cffcae6ad39abe8ad1c3db2f27f5a6
zhangxin <zhangxin8069@qq.com>
2024-10-22 10:28:52 +0000

    2024-10-22-10-28-52

--- lib/v20241023/setup.sh
+++ lib/v20241023/setup.sh
@@ -1,49 +1,50 @@
 ######
 # computer:
-## docker run -itd --gpus all --name v20241023 -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=all -v /c/Users/zhang/v20241023:/home/vscode/v20241023 mcr.microsoft.com/devcontainers/base:noble
+## docker run -itd --gpus all --name v20241023 -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=all -v /c/Users/zhang/v20241023:/home/vscode/v20241023 docker.io/nvidia/cuda:12.4.0-devel-ubuntu22.04
 # laptop:
-## docker run -itd --name v20241023 -v /c/Users/zhangxin/v20241023:/home/vscode/v20241023 mcr.microsoft.com/devcontainers/base:noble
+## docker run -itd --gpus all --name v20241023 -v /c/Users/zhang/v20241023:/home/vscode/v20241023 docker.io/nvidia/cuda:12.4.0-devel-ubuntu22.04
 ######
-sudo apt update
+apt update
 ######
-sudo apt install htop btop nvtop
-sudo apt install python3-dev
-sudo apt install cmake 
-sudo apt install openmpi-bin openmpi-common libopenmpi-dev
-sudo apt install nvidia-cuda-dev
+apt install vim
+apt install htop btop nvtop
+apt install python3-dev
+apt install cmake
+apt install openmpi-bin openmpi-common libopenmpi-dev
+# apt --fix-broken install
+# dpkg --configure -a 
 ######
-# pushd ~
+pushd ~
+# apt install zsh wget
+# sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 # git clone https://gitee.com/zhangxin8069/configure.git
-# pushd ./configure
-# bash ./scripts/script_alias.sh
-# bash ./bin/sh_init.sh
-# cp ./lib/v20241023/env.sh ~/env.sh
+pushd ./configure
+bash ./scripts/script_alias.sh
+cp ./lib/v20241023/env.sh ~/env.sh
+popd
+mv ./.bashrc .bashrc.bak
+mv ./.zshrc .zshrc.bak
+mv ./.oh-my-zsh .oh-my-zsh.bak
+ln -s ${work_path}/_bashrc .bashrc
+ln -s ${work_path}/v20241023/_zshrc .zshrc
+ln -s ${work_path}/_oh-my-zsh .oh-my-zsh
+# ######
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+# dpkg -i cuda-keyring_1.0-1_all.deb
+# apt update
+# apt install libnccl2=2.20.5-1+cuda12.4 libnccl-dev=2.20.5-1+cuda12.4
+# # apt install nsight-systems-target
+# ######
+# wget https://gitee.com/zhangxin8069/quda_packages/raw/main/quda-develop.tar.gz
+# tar -xzf quda-develop.tar.gz
+# mv quda-develop quda
+# pushd ./quda
+# mkdir build -p && pushd build
+# cmake .. -DQUDA_DIRAC_DOMAIN_WALL=OFF -DQUDA_CLOVER_DYNAMIC=OFF -DQUDA_CLOVER_RECONSTRUCT=OFF -DQUDA_DIRAC_NDEG_TWISTED_CLOVER=OFF -DQUDA_DIRAC_NDEG_TWISTED_MASS=OFF -DQUDA_DIRAC_TWISTED_CLOVER=OFF -DQUDA_DIRAC_TWISTED_MASS=OFF -DQUDA_INTERFACE_MILC=OFF -DQUDA_LAPLACE=ON -DQUDA_MULTIGRID=ON -DQUDA_GPU_ARCH=sm_80 -DQUDA_MPI=ON
+# cmake --build . -j6
+# cmake --install .
 # popd
-######
-git clone https://github.com/NVIDIA/nccl.git
-pushd ./nccl
-make -j src.build NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80"
-sudo apt install build-essential devscripts debhelper fakeroot
-# Build NCCL deb package
-make pkg.debian.build
-ls build/pkg/deb/
-popd
-git clone https://github.com/NVIDIA/nccl-tests.git
-pushd ./nccl-tests
-make
-./build/all_reduce_perf -b 8 -e 256M -f 2 -g 1
-# ./build/all_reduce_perf -b 8 -e 256M -f 2 -g <ngpus>
-popd
-wget https://gitee.com/zhangxin8069/quda_packages/raw/main/quda-develop.tar.gz
-tar -xzf quda-develop.tar.gz
-mv quda-develop quda
-pushd ./quda
-mkdir build -p && pushd build
-cmake .. -DQUDA_DIRAC_DOMAIN_WALL=OFF -DQUDA_CLOVER_DYNAMIC=OFF -DQUDA_CLOVER_RECONSTRUCT=OFF -DQUDA_DIRAC_NDEG_TWISTED_CLOVER=OFF -DQUDA_DIRAC_NDEG_TWISTED_MASS=OFF -DQUDA_DIRAC_TWISTED_CLOVER=OFF -DQUDA_DIRAC_TWISTED_MASS=OFF -DQUDA_INTERFACE_MILC=OFF -DQUDA_LAPLACE=ON -DQUDA_MULTIGRID=ON -DQUDA_GPU_ARCH=sm_80 -DQUDA_MPI=ON
-cmake --build . -j6
-cmake --install .
-popd
-popd
+# popd
 popd
 ######
 
