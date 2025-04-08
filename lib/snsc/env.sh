3898b4e0e8700053e63fc4558ef236f109a31d12
zhangxin <zhangxin8069@qq.com>
2025-04-08 14:15:58 +0800

    2025-04-08-14-15-56

--- lib/snsc/env.sh
+++ lib/snsc/env.sh
@@ -1,31 +1,24 @@
 # BASH
 # unset
-
 # MODULE
 module purge
 module load gcc/10.3.0-gcc-4.8.5 
 module load cuda/11.4.4-gcc-10.3.0
 module load git/2.40.0-gcc-10.3.0
 module load openmpi/4.1.5-gcc-10.3.0
-module load cmake/3.22.2-gcc-10.3.0
 module load ncurses/6.2-intel-2022.0.2
-module load intel-oneapi/compiler-rt/2024.0.0 
+module load cmake/3.22.2-gcc-10.3.0
+module load gmake/4.4.1-oneapi-2024.0.0
 module load python/3.9.10-gcc-10.3.0
 module list
-
 # EXPORT
 export PATH=/public/home/zhangxin/sbin:$PATH
 export LD_LIBRARY_PATH=/public/home/zhangxin/slib:$LD_LIBRARY_PATH
 export PYTHONPATH=/public/home/zhangxin/.local/lib/python3.9/site-packages:$PYTHONPATH
-
 # ALIAS
 alias python="python3.9"
 alias pip="pip3.9"
-
 # SOURCE
-# pushd /public/home/zhangxin/qcu
-# source ./snsc-env.sh
-# popd
 pushd /public/home/zhangxin/PyQCU
 source ./env.sh
 popd
