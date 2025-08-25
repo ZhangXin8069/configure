ba473cd656e010d26f6b8bd92dce26196cc5e209
zhangxin <zhangxin8069@qq.com>
2025-08-25 11:37:02 +0800

    2025-08-25-11-37-02

---
+++ lib/dcu-v20250825/env.sh
@@ -0,0 +1,16 @@
+# BASH
+# unset
+pushd ~/configure
+source ./env.sh
+popd
+clear
+# MODULE
+module purge
+module load sghpc-mpi-gcc-mlnx/25.6
+module load compiler/cmake/3.25.3  
+module list
+
+# EXPORT
+~                                                                                                                                                                                   
+~                                                                                                                                                                                   
+~                     
