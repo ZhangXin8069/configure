4085a6b61df8460bfbf31df36305ea694e6e01a1
zhangxin <zhangxin8069@qq.com>
2025-08-25 05:56:45 +0000

    2025-08-25-05-56-45

--- lib/dcu-v20250825/env.sh
+++ lib/dcu-v20250825/env.sh
@@ -7,21 +7,31 @@ clear
 # MODULE
 module purge
 module load sghpc-mpi-gcc-mlnx/25.6
-module load compiler/cmake/3.25.3 
+module load compiler/cmake/3.25.3
 module load compiler/dtk/25.04
 module load sghpcdas/25.6
 module list
 
 # EXPORT
-pushd /opt/dtk-25.04/cuda
+export CUPY_INSTALL_USE_HIP=1
+export OMPI_MCA_opal_cuda_support=0
+export ROCM_HOME=/opt/dtk-25.04
+pushd ${ROCM_HOME}
+source ./env.sh
+pushd ./cuda
 source ./env.sh
 popd
+popd
+export HIPCC=hipcc
+export CC=hipcc
+export CXX=hipcc
+export HCC_AMDGPU_TARGET=gfx906
 
 # CONDA
 # conda init bash
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/public/home/sghpc_sdk/Linux_x86_64/25.6/das/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/public/home/sghpc_sdk/Linux_x86_64/25.6/das/conda/bin/conda' 'shell.bash' 'hook' 2>/dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
