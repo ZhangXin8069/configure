2139e0ed0b904eb1ae73e6c009d39ac8c96c9b75
zhangxin <zhangxin8069@qq.com>
2025-08-25 12:35:15 +0800

    2025-08-25-12-35-15

--- lib/dcu-v20250825/env.sh
+++ lib/dcu-v20250825/env.sh
@@ -11,6 +11,18 @@ module load compiler/cmake/3.25.3
 module list
 
 # EXPORT
-~                                                                                                                                                                                   
+# export CUPY_INSTALL_USE_HIP=1
+# export OMPI_MCA_opal_cuda_support=0
+# export ROCM_HOME=/public/sugon/software/compiler/dtk-23.04
+# pushd ${ROCM_HOME}
+# source ./env.sh
+# pushd ./cuda
+# source ./env.sh
+popd
+popd
+export HIPCC=hipcc
+export CC=hipcc
+export CXX=hipcc
+#export HCC_AMDGPU_TARGET=gfx906
 ~                                                                                                                                                                                   
 ~                     
