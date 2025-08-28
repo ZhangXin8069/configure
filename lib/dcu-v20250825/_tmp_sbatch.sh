8f50ac5e707fc2a496ab21b7a2d76a9f6d4c7b2f
zhangxin <zhangxin8069@qq.com>
2025-08-28 17:16:11 +0800

    2025-08-28-17-16-10

---
+++ lib/dcu-v20250825/_tmp_sbatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J IMP_ZHANGXIN #作业名称
+#SBATCH -p tmplarge #队列名称
+#SBATCH -N 1 #需要几个节点
+#SBATCH --ntasks-per-node=8 #单节点多少个rank
+#SBATCH --cpus-per-task=16 #每个rank 需要多少个cpu核
+#SBATCH --gres=dcu:8 #单节点多少张dcu卡
+#SBATCH -o %j #输出日志
+#SBATCH -e %j #错误日志
+module purge #清空module
+source env.sh
+pushd ~/PyQCU/test
+bash test.clover.bistabcg-npX-test6.sh
+popd
