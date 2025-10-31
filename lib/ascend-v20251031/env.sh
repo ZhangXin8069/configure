eb1ba1c090202d1ed6cdc0cba13e27e433738c70
zhangxin <zhangxin8069@qq.com>
2025-10-31 08:54:12 +0000

    2025-10-31-08-54-12

--- lib/ascend-v20251031/env.sh
+++ lib/ascend-v20251031/env.sh
@@ -1,4 +1,3 @@
-
 pushd /home/phyww/zhangxin
 pushd ./configure
 source env.sh
@@ -14,3 +13,18 @@ export ASCEND_AICPU_PATH=${ASCEND_TOOLKIT_HOME}
 export ASCEND_OPP_PATH=${ASCEND_TOOLKIT_HOME}/opp
 export TOOLCHAIN_HOME=${ASCEND_TOOLKIT_HOME}/toolkit
 export ASCEND_HOME_PATH=${ASCEND_TOOLKIT_HOME}
+# wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py311_25.7.0-2-Linux-aarch64.sh
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/phyww/zhangxin/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/phyww/zhangxin/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/phyww/zhangxin/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/phyww/zhangxin/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
