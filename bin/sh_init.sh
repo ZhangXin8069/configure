0036b06699024688ff426e35a87272e23dc56c3c
zhangxin <zhangxin8069@qq.com>
2025-10-31 05:52:03 +0000

    2025-10-31-05-52-03

--- bin/sh_init.sh
+++ bin/sh_init.sh
@@ -22,6 +22,10 @@ echo "ln -s ${work_path}/_zshrc .zshrc"
 echo "ln -s ${work_path}/_oh-my-zsh .oh-my-zsh"
 mv ./.bashrc .bashrc.bak
 mv ./.zshrc .zshrc.bak
+# apt install zsh wget
+# sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+mkdir -p .oh-my-zsh
+mv .oh-my-zsh .oh-my-zsh.bak
 ln -s ${work_path}/_bashrc .bashrc
 ln -s ${work_path}/_zshrc .zshrc
 ln -s ${work_path}/_oh-my-zsh .oh-my-zsh
