848a71f1b3cae7e7882622e277b767a3c1a55886
zhangxin <zhangxin8069@qq.com>
2025-11-01 02:54:13 +0000

    2025-11-01-02-54-13

--- lib/ascend-v20251031/env.sh
+++ lib/ascend-v20251031/env.sh
@@ -3,17 +3,12 @@ pushd ./configure
 source env.sh
 popd
 popd
-export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64:/usr/local/Ascend/driver/lib64/common:/usr/local/Ascend/driver/lib64/driver:$LD_LIBRARY_PATH
-export ASCEND_TOOLKIT_HOME=/home/phyww/Ascend/ascend-toolkit/latest
-export LD_LIBRARY_PATH=${ASCEND_TOOLKIT_HOME}/lib64:${ASCEND_TOOLKIT_HOME}/lib64/plugin/opskernel:${ASCEND_TOOLKIT_HOME}/lib64/plugin/nnengine:${ASCEND_TOOLKIT_HOME}/opp/built-in/op_impl/ai_core/tbe/op_tiling/lib/linux/$(arch):$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=${ASCEND_TOOLKIT_HOME}/tools/aml/lib64:${ASCEND_TOOLKIT_HOME}/tools/aml/lib64/plugin:$LD_LIBRARY_PATH
-export PYTHONPATH=${ASCEND_TOOLKIT_HOME}/python/site-packages:${ASCEND_TOOLKIT_HOME}/opp/built-in/op_impl/ai_core/tbe:$PYTHONPATH
-export PATH=${ASCEND_TOOLKIT_HOME}/bin:${ASCEND_TOOLKIT_HOME}/compiler/ccec_compiler/bin:${ASCEND_TOOLKIT_HOME}/tools/ccec_compiler/bin:$PATH
-export ASCEND_AICPU_PATH=${ASCEND_TOOLKIT_HOME}
-export ASCEND_OPP_PATH=${ASCEND_TOOLKIT_HOME}/opp
-export TOOLCHAIN_HOME=${ASCEND_TOOLKIT_HOME}/toolkit
-export ASCEND_HOME_PATH=${ASCEND_TOOLKIT_HOME}
 # wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py311_25.7.0-2-Linux-aarch64.sh
+# https://www.hiascend.com/document/detail/zh/CANNCommunityEdition/82RC1/softwareinst/instg/instg_quick.html
+# conda config --add channels https://repo.huaweicloud.com/ascend/repos/conda/ 
+# conda install ascend::cann-toolkit
+source /home/phyww/zhangxin/miniconda3/Ascend/ascend-toolkit/set_env.sh
+# conda install ascend::a3-cann-kernels
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/home/phyww/zhangxin/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
@@ -28,4 +23,3 @@ else
 fi
 unset __conda_setup
 # <<< conda initialize <<<
-export TORCH_DEVICE_BACKEND_AUTOLOAD=1
