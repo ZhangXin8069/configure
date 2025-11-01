47198f18bbd07adf748a5de28e538db3f269cb84
zhangxin <zhangxin8069@qq.com>
2025-11-01 07:06:12 +0000

    2025-11-01-07-06-12

--- lib/ascend-v20251031/env.sh
+++ lib/ascend-v20251031/env.sh
@@ -1,9 +1,3 @@
-pushd /home/phyww/zhangxin
-source ./miniconda3/Ascend/ascend-toolkit/set_env.sh
-pushd ./configure
-source env.sh
-popd
-popd
 # wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py311_25.7.0-2-Linux-aarch64.sh
 # https://www.hiascend.com/document/detail/zh/CANNCommunityEdition/82RC1/softwareinst/instg/instg_quick.html
 # conda config --add channels https://repo.huaweicloud.com/ascend/repos/conda/ 
