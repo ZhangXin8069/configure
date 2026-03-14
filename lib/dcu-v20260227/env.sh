483dbca7971823add37f7160dbb1620e74ab7b2c
zhangxin <zhangxin8069@qq.com>
2026-03-14 18:24:59 +0800

    2026-03-14-18-24-58

--- lib/dcu-v20260227/env.sh
+++ lib/dcu-v20260227/env.sh
@@ -1,28 +1,31 @@
-# @CONFIGURE@
-pushd /public/home/scnethpc2623/configure
-source ./env.sh
-popd
 # @@UNALIAS@@
 unalias history
 unalias his
 # @MODULE@
 module purge
 module load anaconda3/2023.09
-module load compiler/dtk/25.04.1
+module load compiler/dtk/25.04.2
 module load compiler/gcc/12.2.0
 module load mpi/openmpi/4.1.8/gcc-12.2.0/mlnx 
 module list
 # @EXPORT@
 export PYTHONPATH=/public/home/scnethpc2623/PyQCU:${PYTHONPATH}
-export CUDA_PATH=${ROCM_PATH}/cuda/cuda-12
-export CUDA_BIN_PATH=$CUDA_PATH/bin
-export PATH=$CUDA_BIN_PATH:${PATH}
-export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$CUDA_PATH/extras/CUPTI/lib64:${LD_LIBRARY_PATH}
-export LIBRARY_PATH=$CUDA_PATH/lib64:$CUDA_PATH/extras/CUPTI/lib64:${LIBRARY_PATH}
-export C_INCLUDE_PATH=$CUDA_PATH/include:$CUDA_PATH/extras/CUPTI/include:${C_INCLUDE_PATH}
-export CPLUS_INCLUDE_PATH=$CUDA_PATH/include:$CUDA_PATH/extras/CUPTI/include:${CPLUS_INCLUDE_PATH}
-export CUDAARCHS=75
+# export CUDA_PATH=${ROCM_PATH}/cuda/cuda-12
+# export CUDA_BIN_PATH=$CUDA_PATH/bin
+# export PATH=$CUDA_BIN_PATH:${PATH}
+# export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$CUDA_PATH/extras/CUPTI/lib64:${LD_LIBRARY_PATH}
+# export LIBRARY_PATH=$CUDA_PATH/lib64:$CUDA_PATH/extras/CUPTI/lib64:${LIBRARY_PATH}
+# export C_INCLUDE_PATH=$CUDA_PATH/include:$CUDA_PATH/extras/CUPTI/include:${C_INCLUDE_PATH}
+# export CPLUS_INCLUDE_PATH=$CUDA_PATH/include:$CUDA_PATH/extras/CUPTI/include:${CPLUS_INCLUDE_PATH}
+# export CUDAARCHS=75
 # @CONDA@
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/main/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/conda-forge/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/msys2/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/bioconda/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/menpo/
+# conda config --set show_channel_urls yes
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/public/software/apps/anaconda3/2023.09/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
@@ -39,6 +42,9 @@ unset __conda_setup
 # <<< conda initialize <<<
 # conda create -n qcu python=3.10.10
 conda activate qcu
-# pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple ninja pytest mpi4py h5py --resume-retries 10
-# wget https://download.sourcefind.cn:65024/file/4/tilelang/DAS1.7/tilelang-0.1.6.post2+das.opt1.dtk25042-cp310-cp310-linux_x86_64.whl
-# pip install tilelang-0.1.6.post2+das.opt1.dtk25042-cp310-cp310-linux_x86_64.whl -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+# conda install -c conda-forge gflags
+# pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple ninja pytest mpi4py h5py numpy==1.26.4 --resume-retries 10
+# wget https://download.sourcefind.cn:65024/file/4/pytorch/DAS1.7/torch-2.7.1+das.opt1.dtk25042-cp310-cp310-manylinux_2_28_x86_64.whl
+# pip install torch-2.7.1+das.opt1.dtk25042-cp310-cp310-manylinux_2_28_x86_64.whl -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+# wget https://download.sourcefind.cn:65024/file/4/tilelang/DAS1.7/tilelang-0.1.6.post2+das.opt1.dtk25042-cp310-cp310-manylinux_2_28_x86_64.whl
+# pip install tilelang-0.1.6.post2+das.opt1.dtk25042-cp310-cp310-manylinux_2_28_x86_64.whl -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
