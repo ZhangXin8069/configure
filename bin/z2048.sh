597a464b63ad57ba6ec6f4154ff19a4b9baaa3ea
zhangxin <zhangxin8069@qq.com>
2026-03-20 20:28:24 +0800

    2026-03-20-20-28-24

--- bin/z2048.sh
+++ bin/z2048.sh
@@ -5,4 +5,5 @@ _PATH=$(
 _NAME=$(basename "$0")
 echo "###${_NAME} in ${_PATH} is running...:$(date "+%Y-%m-%d-%H-%M-%S")###"
 ${_PATH}/../lib/_package/linux/2048
+clear
 echo "###${_NAME} in ${_PATH} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
