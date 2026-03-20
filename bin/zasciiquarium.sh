f56365897397e8144683bfbdf96b500f412f849b
zhangxin <zhangxin8069@qq.com>
2026-03-20 20:31:26 +0800

    2026-03-20-20-31-26

--- bin/zasciiquarium.sh
+++ bin/zasciiquarium.sh
@@ -5,5 +5,4 @@ _PATH=$(
 _NAME=$(basename "$0")
 echo "###${_NAME} in ${_PATH} is running...:$(date "+%Y-%m-%d-%H-%M-%S")###"
 curl "http://asciiquarium.live?cols=$(tput cols)&rows=$(tput lines)"
-clear
 echo "###${_NAME} in ${_PATH} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
