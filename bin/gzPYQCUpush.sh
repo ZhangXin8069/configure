a92987b3ad94ad7b23fc027fddaf4f7a461fb84a
zhangxin <zhangxin8069@qq.com>
2026-04-03 15:22:56 +0800

    2026-04-03-15-22-56

---
+++ bin/gzPYQCUpush.sh
@@ -0,0 +1,11 @@
+_PATH=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+_NAME=$(basename "$0")
+echo "###${_NAME} in ${_PATH} is running...:$(date "+%Y-%m-%d-%H-%M-%S")###"
+pushd ${_PATH}/../../PyQCU
+git stash push
+bash ${_PATH}/gpush.sh
+popd
+echo "###${_NAME} in ${_PATH} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
