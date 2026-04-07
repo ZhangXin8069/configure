cb9b21dee4a8eb1d4297d70ecfa248718b3790b5
zhangxin <zhangxin8069@qq.com>
2026-04-07 18:28:02 +0800

    2026-04-07-18-28-02

--- lib/aistudio-v20260407/env.sh
+++ lib/aistudio-v20260407/env.sh
@@ -1,22 +1,7 @@
 ## CONDA
 # conda create --name qcu python=3.11 cmake zsh htop
 # cp -r /home/aistudio/.conda/envs/ lib/
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/aistudio/miniconda3/bin/conda' 'shell.bash' 'hook' 2>/dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/aistudio/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/aistudio/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/aistudio/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
 conda activate /home/aistudio/lib/envs/qcu
-# <<< conda initialize <<<
 
 ## TORCH
 # pip3 install torch-npu==2.1.0.post17 pyyaml numpy==1.26.4
