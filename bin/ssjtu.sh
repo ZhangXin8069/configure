25d6a37c5d1fa35c9bab3dbcb0e390ae47fb4216
zhangxin <zhangxin8069@qq.com>
2026-04-07 14:27:54 +0800

    2026-04-07-14-27-54

--- bin/ssjtu.sh
+++ bin/ssjtu.sh
@@ -5,4 +5,5 @@ _PATH=$(
 _NAME=$(basename "$0")
 echo "###${_NAME} in ${_PATH} is running...:$(date "+%Y-%m-%d-%H-%M-%S")###"
 ssh weiwang@bl-0.inpac.sjtu.edu.cn
+# scp weiwang@bl-0.inpac.sjtu.edu.cn:/home/weiwang/profiler/trace_20260407140930_0.json .
 echo "###${_NAME} in ${_PATH} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
