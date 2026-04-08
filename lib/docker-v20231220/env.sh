0906870bacf5aa2be875816bb237492456051406
zhangxin <zhangxin8069@qq.com>
2026-04-08 15:27:21 +0000

    2026-04-08-15-27-21

--- lib/docker-v20231220/env.sh
+++ lib/docker-v20231220/env.sh
@@ -4,6 +4,7 @@ export PYTHONPATH=/root/lib/PyQuda-master:${PYTHONPATH}
 export MPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/PyQCU/lib
 alias mpirun='mpirun --allow-run-as-root'
 alias python='python3 -u'
