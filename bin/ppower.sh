0e2f2fc5e66b27ae1377445f73a12f3e0e20a06e
zhangxin <zhangxin8069@qq.com>
2026-05-25 22:06:14 +0800

    2026-05-25-22-06-14

---
+++ bin/ppower.sh
@@ -0,0 +1,8 @@
+_PATH=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+_NAME=$(basename "$0")
+echo "###${_NAME} in ${_PATH} is running...:$(date "+%Y-%m-%d-%H-%M-%S")###"
+sudo powermetrics --samplers all
+echo "###${_NAME} in ${_PATH} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
