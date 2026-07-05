1143297ed5d5a3c5722cc73ddf0276491b216843
zhangxin <zhangxin8069@qq.com>
2026-07-05 07:04:33 +0000

    2026-07-05-07-04-33

---
+++ bin/vscode_unset.sh
@@ -0,0 +1,11 @@
+_PATH=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+_NAME=$(basename "$0")
+echo "###${_NAME} in ${_PATH} is running...:$(date "+%Y-%m-%d-%H-%M-%S")###"
+pushd ~
+mv ./.vscode .vscode."$(date "+%Y-%m-%d-%H-%M-%S")".bak
+mv ./.vscode-server .vscode-server."$(date "+%Y-%m-%d-%H-%M-%S")".bak
+mv ./.vscode-remote-containers .vscode-remote-containers."$(date "+%Y-%m-%d-%H-%M-%S")".bak
+echo "###${_NAME} in ${_PATH} is done......:$(date "+%Y-%m-%d-%H-%M-%S")###"
