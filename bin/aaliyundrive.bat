14a3a7b610a5e44cc34da88f625089a7be02d475
zhangxin <zhangxin8069@qq.com>
2026-07-21 09:41:16 +0000

    2026-07-21-09-41-16

---
+++ bin/aaliyundrive.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+title Aliyun Drive
+
+echo ============================================================
+echo   Aliyun Drive
+echo ============================================================
+echo.
+echo   Opening https://www.aliyundrive.com/drive/file/all ...
+echo.
+
+start "" "https://www.aliyundrive.com/drive/file/all"
+
+endlocal
+exit /b 0
