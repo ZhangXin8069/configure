5614df3b37cef3fa5a69a65587403af2873c4f52
zhangxin <zhangxin8069@qq.com>
2026-07-21 09:41:16 +0000

    2026-07-21-09-41-16

---
+++ bin/bbilibili.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+title Bilibili
+
+echo ============================================================
+echo   Bilibili
+echo ============================================================
+echo.
+echo   Opening https://www.bilibili.com/ ...
+echo.
+
+start "" "https://www.bilibili.com/"
+
+endlocal
+exit /b 0
---
+++ bin/ggitee.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+title Gitee Projects
+
+echo ============================================================
+echo   Gitee - zhangxin8069 Projects
+echo ============================================================
+echo.
+echo   Opening https://gitee.com/zhangxin8069/projects ...
+echo.
+
+start "" "https://gitee.com/zhangxin8069/projects"
+
+endlocal
+exit /b 0
