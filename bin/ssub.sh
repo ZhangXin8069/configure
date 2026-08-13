751a24be3836869508b77b75858e542aa26bae15
zhangxin <zhangxin8069@qq.com>
2026-08-14 07:48:32 +0800

    2026-08-14-07-48-32

--- bin/ssub.sh
+++ bin/ssub.sh
@@ -11,8 +11,9 @@ echo "#!/bin/bash
 #SBATCH --partition=gpu-debug
 #SBATCH --nodes=1
 # #SBATCH --cpus-per-task=1
-#SBATCH -n 2
-# # SBATCH --ntasks-per-node=48
+# #SBATCH -n 2
+#SBATCH -n 1
+# #SBATCH --ntasks-per-node=48
 # #SBATCH --nodelist=node[3,4]
 # #SBATCH --exclude=node[1,5-6]
 #SBATCH --time=00-00:30:00
@@ -20,7 +21,8 @@ echo "#!/bin/bash
 #SBATCH --error=ssub.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zhangxin8069@qq.com
-#SBATCH --gres=gpu:2
+# #SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 source ${HOME}/env.sh
 " >.ssub.sh
 echo ${command} >>.ssub.sh
